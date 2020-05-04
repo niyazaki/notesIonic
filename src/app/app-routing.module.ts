396bd9c647e0c0442e4a9cfdc0dafed832b6a597
16131 <zakitylox@gmail.com>
2020-05-04 04:57:22 +0200

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'categories',
+    loadChildren: () => import('./pages/categories/categories.module').then( m => m.CategoriesPageModule)
+  },
+  {
+    path: 'category-details',
+    loadChildren: () => import('./pages/category-details/category-details.module').then( m => m.CategoryDetailsPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/category-details/category-details.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-details',
+  templateUrl: './category-details.page.html',
+  styleUrls: ['./category-details.page.scss'],
+})
+export class CategoryDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
