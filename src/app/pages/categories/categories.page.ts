3e8d1f4ee455b2ee3f95c0c73139348574ff5785
16131 <zakitylox@gmail.com>
2020-05-13 01:13:12 +0200

    CRUD and slider for categories totally working. We get a bunch of errors when deleting even everything is fine.

--- src/app/pages/categories/categories.page.ts
+++ src/app/pages/categories/categories.page.ts
@@ -1,36 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
-import { CategoriesService} from '../../services/categories.service';
+import { Component, OnInit } from "@angular/core";
+import { LoadingController } from "@ionic/angular";
+import { CategoriesService } from "../../services/categories.service";
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.page.html',
-  styleUrls: ['./categories.page.scss'],
+  selector: "app-categories",
+  templateUrl: "./categories.page.html",
+  styleUrls: ["./categories.page.scss"],
 })
 export class CategoriesPage implements OnInit {
+  constructor(
+    public api: CategoriesService,
+    public loadingController: LoadingController
+  ) {}
 
-  constructor(public api: CategoriesService, public loadingController: LoadingController) { }
-  
   categories: any;
 
   async getCategories() {
     const loading = await this.loadingController.create();
     await loading.present();
-    await this.api.getCategory()
-      .subscribe(res => {
+    await this.api.getCategory().subscribe(
+      (res) => {
         console.log(res);
         this.categories = res;
         loading.dismiss();
-      }, err => {
+      },
+      (err) => {
         console.log(err);
         loading.dismiss();
-      });
+      }
+    );
   }
 
-
-
-
+  async deleteCategory(id) {
+    const loading = await this.loadingController.create();
+    await loading.present();
+    await this.api.deleteCategory(id).subscribe(
+      (res) => {
+        console.log(res);
+        this.categories = res;
+        loading.dismiss();
+      },
+      (err) => {
+        console.log(err);
+        loading.dismiss();
+      }
+    );
+    this.getCategories();
+  }
   ngOnInit() {
     this.getCategories();
   }
-
 }
