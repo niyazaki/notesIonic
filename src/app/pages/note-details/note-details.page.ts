1ff28cecf219353dad653a3e394e1015709776c7
16131 <zakitylox@gmail.com>
2020-05-13 07:37:22 +0200

    RD functionnal. Reading notes details is also functionnal even if we have to click several times (getting errors)

--- src/app/pages/note-details/note-details.page.ts
+++ src/app/pages/note-details/note-details.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { LoadingController } from "@ionic/angular";
+import { NotesService } from "../../services/notes.service";
+import { ActivatedRoute } from '@angular/router';
 @Component({
-  selector: 'app-note-details',
-  templateUrl: './note-details.page.html',
-  styleUrls: ['./note-details.page.scss'],
+  selector: "app-notes",
+  templateUrl: "./note-details.page.html",
+  styleUrls: ["./note-details.page.scss"],
 })
 export class NoteDetailsPage implements OnInit {
+  constructor(
+    public api: NotesService,
+    public loadingController: LoadingController,
+    private route: ActivatedRoute,
+  ) {this.getNote(this.route.snapshot.paramMap.get('id'))}
 
-  constructor() { }
+  note: any;
 
-  ngOnInit() {
+  async getNote(id) {
+    const loading = await this.loadingController.create({
+      message: "Loading",
+    });
+    await loading.present();
+    await this.api.getNoteById(id).subscribe(
+      (data) => {
+        this.note = {
+          title: data.title,
+          content: data.content,
+          date: data.date,
+          category: data.category,
+        };
+        loading.dismiss();
+      },
+      (err) => {
+        console.log(err);
+        loading.dismiss();
+      }
+    );
   }
 
+  ngOnInit() {}
 }
